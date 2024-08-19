c0cef4ec6eaa4f4728ea2069225c906ca6f07515
tanhdev98 <tanhdev98@gmail.com>
2024-08-19 10:24:25 +0700

    update

---
+++ src/utils/customHook.ts
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+export const useHasMounted = () => {
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  return hasMounted;
+};
